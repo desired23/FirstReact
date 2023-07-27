d6a7adeb67c170e9518b0967134cfda114bf9eeb
desired23 <huydang2312003@gmail.com>
2023-07-27 22:32:11 +0700

    update api

--- src/utils/api/instance.tsx
+++ src/utils/api/instance.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL: "http://127.0.0.1:8088/api/",
+    baseURL: "https://portfolio-zsir.onrender.com/api/",
 })
 export default instance;
